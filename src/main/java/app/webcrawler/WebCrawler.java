3aff51fd371aeb4664419c5b462674bf70e2b63b
Aleksandra <aleks.grunwald@gmail.com>
2019-06-11 03:12:54 +0200

    JSoup gets url source code and gather a hrefs

---
+++ src/main/java/app/webcrawler/WebCrawler.java
@@ -0,0 +1,36 @@
+package app.webcrawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebCrawler {
+
+    public List<String> getLinksFromUrl(String url) throws Exception{
+        List<String> foundHrefs = new ArrayList<String>();
+        List<String> filteredHrefs = new ArrayList<String>();
+
+        Document webpage = Jsoup.connect(url).get();
+        Elements aHrefs = webpage.select("a");
+        for(Element link : aHrefs) {
+            foundHrefs.add(link.attr("href"));
+        }
+
+        foundHrefs.stream()
+                .forEach((elem) -> {
+                    System.out.print(elem);
+                    if(filteredHrefs.indexOf(elem) == -1) {
+                        filteredHrefs.add(elem);
+                    }
+                });
+        }
+
+        return
+    }
+
+
+}
