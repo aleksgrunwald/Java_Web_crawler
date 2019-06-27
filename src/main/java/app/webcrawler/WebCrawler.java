6fe4a13949d054988f227de8b4d543d22696708e
Aleksandra <aleks.grunwald@gmail.com>
2019-06-27 19:51:27 +0200

    Pom config changes

--- src/main/java/app/webcrawler/WebCrawler.java
+++ src/main/java/app/webcrawler/WebCrawler.java
@@ -10,27 +10,26 @@ import java.util.List;
 
 public class WebCrawler {
 
-    public List<String> getLinksFromUrl(String url) throws Exception{
+    public List<String> getLinksFromUrl(String url) throws Exception {
         List<String> foundHrefs = new ArrayList<String>();
         List<String> filteredHrefs = new ArrayList<String>();
 
         Document webpage = Jsoup.connect(url).get();
         Elements aHrefs = webpage.select("a");
-        for(Element link : aHrefs) {
+        for (Element link : aHrefs) {
             foundHrefs.add(link.attr("href"));
         }
 
-//        foundHrefs.stream()
-//                .forEach((elem) -> {
-//                    System.out.print(elem);
+        foundHrefs.stream()
+                .forEach((elem) -> {
+                    System.out.print(elem);
 //                    if(filteredHrefs.indexOf(elem) == -1) {
 //                        filteredHrefs.add(elem);
 //                    }
-//                });
-//        }
+                });
+        }
 
-        return foundHrefs
+
+        return foundHrefs;
     }
-
-
 }
